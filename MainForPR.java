e77ac743adb38353d410e0e942489989e2a57257
kino41 <formaggio-55511@i.softbank.jp>
2024-02-03 13:27:27 +0900

    PR用のファイルを作成

---
+++ MainForPR.java
@@ -0,0 +1,19 @@
+# MainForPR
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = List.of("grape", "peach", "apple", "banana", "orange");
+        List<String> sortedResult = names.stream().sorted().toList();
+        System.out.println(sortedResult);
+
+        List<String> result = names.stream()
+                .filter(name -> name.contains("p"))
+                .filter(name -> name.startsWith("a"))
+                .toList();
+        System.out.println("p‚ğŠÜ‚İAa‚©‚çn‚Ü‚é‰Ê•¨‚Í" + result);
+    }
+}
